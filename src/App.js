c0abf14918e0de966d332ea69fd793bff1329349
Kritesh77 <55019533+Kritesh77@users.noreply.github.com>
2020-12-14 18:59:53 +0530

    yo
    yo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header.js';
+import Mainwrap from './components/Mainwrap.js'
+import Slider1 from './components/Slider1.js'
+import Slider2 from './components/Slider2.js'
+function App() {
+  return (
+    <div>
+      <Header />
+      <Mainwrap />
+      <Slider1 />
+      <Slider2 />
+    </div>
+  );
+}
+
+export default App;
